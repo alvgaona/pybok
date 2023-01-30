529e38bbebdf7449cb0b98ebee0531b12935007e
Alvaro <alvgaona@gmail.com>
2023-01-29 22:52:26 -0300

    Support decorator args (?)

--- pybok/base.py
+++ pybok/base.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 class Base(ABC):
     fields = {}
+    kwargs = None
+    args = None
 
     def _init_fields(cls):
         fields = {}
@@ -14,11 +17,26 @@ class Base(ABC):
         
         return fields
 
-    def __new__(cls, arg):
-        cls.fields = cls._init_fields(arg)
-        cls.decorate(cls, arg)
-        return arg
+    def __new__(cls, arg=None, *args, **kwargs):
+        """
+        HACK: I really don't know how this is working but it works
+        to support decorators with args.
+
+        All I know that for each decorator, its __new__ method is called
+        twice when it receives arguments. For some reason, this makes it
+        possible that the decorator method gets the decorated class, and
+        the decorator arguments.
+        """
+        if arg is not None:
+            cls.fields = cls._init_fields(arg)
+            cls.decorate(cls, arg)
+            return arg
+        else:
+            cls.args = args
+            cls.kwargs = kwargs
+            return cls
+    
     
     @abstractmethod
-    def decorate(cls, arg):
+    def decorate(cls, arg, **kwargs):
         pass
--- pybok/getter.py
+++ pybok/getter.py
@@ -2,6 +2,10 @@ from pybok.base import Base
 from pybok.decorators import _getter_fn
 
 class Getter(Base):
-    def decorate(cls, arg):
+     def decorate(cls, arg):
+        print(arg)
+        print(cls.kwargs)
+        print(cls.args)
+
         for field in cls.fields:
             setattr(arg, f'get_{field}', _getter_fn(field))
